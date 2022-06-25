ef5638b14d7bd06104b4873898067dee7e3e6bd3
Ras96 <asymptote.k.k@gmail.com>
2022-06-25 10:38:54 +0900

    :sparkles: add color

--- init.sh
+++ init.sh
@@ -3,56 +3,60 @@
 GOLANG_VERSION=1.18.3
 MEMBERS=(tesso57 toshi-pono Ras96)
 
+echoe() {
+  echo -e "\e[44m$1\e[m"
+}
+
 # read args
 read -p "project root (ex. /home/isucon/webapp) > " PROJECT_ROOT
 read -p "project repo url (ex. git@github.com:hoge/isuconXXq.git) > " PROJECT_REPO_URL
 
 # update & upgrade apt packages
-echo "Updating & upgrading apt packages..."
+echoe "Updating & upgrading apt packages..."
 apt update -y
 apt upgrade -y
-echo "Done!!"
+echoe "Done!!"
 
 # install apt tools
-echo "Installing apt tools..."
+echoe "Installing apt tools..."
 apt install -y percona-toolkit htop git curl wget
-echo "Done!!"
+echoe "Done!!"
 
 # Add commands to $PATH
-echo "Adding commands to \$PATH..."
+echoe "Adding commands to \$PATH..."
 echo "export PATH=\$PATH:$PWD/bin" >> ~/.bashrc
-echo "Done!!"
+echoe "Done!!"
 
 # install asdf
-echo "Installing asdf..."
+echoe "Installing asdf..."
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 echo "source ~/.asdf/asdf.sh" >> ~/.bashrc \
 && echo "source ~/.asdf/completions/asdf.bash" >> ~/.bashrc \
 && source ~/.bashrc
-echo "Done!!"
+echoe "Done!!"
 
 # install golang
-echo "Installing golang..."
+echoe "Installing golang..."
 asdf plugin add golang
 asdf install golang $GOLANG_VERSION
 asdf global golang $GOLANG_VERSION
-echo "Done!!"
+echoe "Done!!"
 
 # Add commands to $PATH
-echo "Adding commands to \$PATH..."
+echoe "Adding commands to \$PATH..."
 echo "export PATH=\$PATH:$PWD/bin" >> ~/.bashrc
-echo "Done!!"
+echoe "Done!!"
 
 # sync public keys
-echo "Syncing public keys..."
+echoe "Syncing public keys..."
 mkdir -p ~/.ssh
 for member in ${MEMBERS[@]}; do
   curl https://github.com/$member.keys >> ~/.ssh/authorized_keys
 done
-echo "Done!!"
+echoe "Done!!"
 
 # initialize git
-echo "Initializing git..."
+echoe "Initializing git..."
 git config --global user.name "server"
 git config --global user.email "example@gmail.com"
 git config --global core.editor "vim"
@@ -61,5 +65,5 @@ git config --global init.defaultbranch main
 cd $PROJECT_ROOT \
   && git init \
   && git remote add origin $PROJECT_REPO_URL
-echo "Done!!"
+echoe "Done!!"
 echo "Please push your code to Github."
